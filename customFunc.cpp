0f06072e085f3e1b7855e694df3f6ff4d4b23208
Jubayer-Talukder-Shuvo <jtshuvo6@gmail.com>
2025-06-20 01:05:52 +0600

    added upperbound func

--- customFunc.cpp
+++ customFunc.cpp
@@ -230,6 +230,18 @@ int lower_bound_my(vector<int>& arr, int x) {
     }
     return l;
 }
+int upper_bound_my(vector<int>& arr, int x) {
+    int l=0, r=arr.size();
+    while(l<r){
+        int mid=l+(r-l)/2;
+        if(arr[mid]<=x){
+            l=mid+1;
+        }else{
+            r=mid;
+        }
+    }
+    return l;
+}
 
 
 
