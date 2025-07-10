102376d65d0b78f8b27d75849c0440e772880144
Jubayer-Talukder-Shuvo <jtshuvo6@gmail.com>
2025-07-11 01:59:14 +0600

    added visited

--- dfs.cpp
+++ dfs.cpp
@@ -3,15 +3,17 @@ using namespace std;
 
 const int N = 1e5 + 10;
 vector<int> g[N];
+bool visited[N];
 
 //dfs
 void dfs(int vertex){
     //Take action on vertex after entering the vertex
-
-   for(int child : g[vertex]){
+    visited[vertex]=true; 
+    for(int child : g[vertex]){
+    if(visited[child]) continue;
     //Take action on child before entering the child node
 
-    dfs(child);
+        dfs(child);
 
     // Take action on child after exciting child node
    }
