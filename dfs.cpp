8f3946d78d523b9face9ab3a07a242da48aa6992
Jubayer-Talukder-Shuvo <jtshuvo6@gmail.com>
2025-07-11 18:12:18 +0600

    added proper formating

--- dfs.cpp
+++ dfs.cpp
@@ -11,11 +11,11 @@ void dfs(int vertex){
     visited[vertex]=true; 
     for(int child : g[vertex]){
     if(visited[child]) continue;
-    //Take action on child before entering the child node
+        //Take action on child before entering the child node
 
         dfs(child);
 
-    // Take action on child after exciting child node
+        // Take action on child after exciting child node
    }
    //Take action on vertex before exciting the vertex
 }
