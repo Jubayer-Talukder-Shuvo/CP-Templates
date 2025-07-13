2005412c76dc1258560c732c46f78fd9945aee72
Jubayer-Talukder-Shuvo <jtshuvo6@gmail.com>
2025-07-13 15:41:07 +0600

    added template in customfunc

--- customFunc.cpp
+++ customFunc.cpp
@@ -5,7 +5,17 @@ using namespace std;
 #define watch(x) cout<<(#x)<<" = " <<(x)<<endl;
 #define test int t; cin>>t; while(t--)
 #define gcin(s) getline(cin, s);
-#define vi  vector<int>
+#define vi vector<int>
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define vll vector<long long>
+#define vv(name, type, rows, cols) vector<vector<type>> name(rows, vector<type>(cols))
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+//bit manipulation
+#define checkbit(x,n) (x&(1LL<<n))
+#define setbit(x,n) (x=(x|(1LL<<n)))
+#define unsetbit(x,n) (x=(x&(~(1LL<<n))))
+#define togglebit(x,n) (x=(x^(1LL<<n)))
 
 //fold shortcut Ctrl+k, Ctrl+0
 //unfold shortcut Ctrl+k, Ctrl+j
