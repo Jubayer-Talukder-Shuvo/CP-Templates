1583831ee7642ea3bd294137c7b12844b63e9642
Jubayer-Talukder-Shuvo <jtshuvo6@gmail.com>
2025-11-28 12:13:18 +0600

    updated miscellaneous

---
+++ Miscellaneous/Josephus_Problem_I.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define f(i, st, ed) for(int i=st; i<=ed; i++)
+#define ll long long
+#define watch(x) cout<<(#x)<<" = " <<(x)<<endl;
+#define test int t; cin>>t; while(t--)
+#define gcin(s) getline(cin, s);
+#define vi vector<int>
+#define vll vector<long long>
+#define pb push_back
+#define all(v) v.begin(), v.end()
+#define vv(name, type, rows, cols) vector<vector<type>> name(rows, vector<type>(cols))
+#define fastio ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define edge(v) g[v].size()
+#define yes cout<<"YES\n"
+#define no cout<<"NO\n"
+#define nl '\n'
+#define pvec(a) do{for(auto &x:a) cout<<x<<" "; cout<<endl;}while(0)
+//bit manipulation
+#define checkbit(x,n) (x&(1LL<<n))
+#define setbit(x,n) (x=(x|(1LL<<n)))
+#define unsetbit(x,n) (x=(x&(~(1LL<<n))))
+#define togglebit(x,n) (x=(x^(1LL<<n)))
+
+const ll M = 1000000007;
+
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+using namespace __gnu_pbds;
+template<typename T> using pbds = tree<T, null_type, less<T>, rb_tree_tag,
+tree_order_statistics_node_update>;
+
+
+void solve(){
+    int n;
+    cin>>n;
+
+    pbds<int> s;
+    f(i, 1, n) s.insert(i);
+
+
+    int idx=1;
+    while(!s.empty()){
+        idx=idx%s.size();
+        auto it=s.find_by_order(idx);
+        cout<<*it<<' ';
+
+        s.erase(it);
+        idx++;
+
+    }
+
+
+
+    cout<<nl;
+
+}
+
+int main(){
+    fastio;
+    solve();
+    return 0;
+}
